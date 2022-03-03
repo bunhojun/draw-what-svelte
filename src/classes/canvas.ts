8a71e66833af44232a885ae3ec10fdf9cb4e6b64
bunhojun <ponnponn.p@gmail.com>
2022-03-03 22:15:00 +0900

    refactor

--- src/classes/canvas.ts
+++ src/classes/canvas.ts
@@ -12,13 +12,9 @@ export class Canvas extends P5 {
 	constructor(element: HTMLElement) {
 		super(initCanvas, element);
 		this.mouseDragged = (): void => {
-			const data = {
-				x: this.mouseX,
-				y: this.mouseY,
-			};
 			this.noStroke();
 			this.fill(0);
-			this.ellipse(data.x, data.y, CANVAS.BRUSH_THICKNESS, CANVAS.BRUSH_THICKNESS);
+			this.ellipse(this.mouseX, this.mouseY, CANVAS.BRUSH_THICKNESS, CANVAS.BRUSH_THICKNESS);
 		};
 	}
 
