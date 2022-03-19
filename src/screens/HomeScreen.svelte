bb30ff627910142e361b82ba0ff2d0e5f404f19b
bunhojun <ponnponn.p@gmail.com>
2022-03-19 23:33:34 +0900

    add test case

---
+++ src/screens/HomeScreen.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { isGameStarted } from "../stores/stores";
+	import { TEST_ID } from "../constants/test-id";
+
+	const onClickStart = () => {
+		isGameStarted.set(true);
+	};
+</script>
+
+<h1>Draw What?</h1>
+<button on:click={onClickStart} data-testid={TEST_ID.StartButton}>start game</button>
