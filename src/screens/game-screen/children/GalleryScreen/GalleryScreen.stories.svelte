7ba31ba7c7343bc5fbf79fa6d38dba534f6739c8
bunhojun <ponnponn.p@gmail.com>
2022-03-30 22:36:39 +0900

    add gallery items

--- src/screens/game-screen/children/GalleryScreen/GalleryScreen.stories.svelte
+++ src/screens/game-screen/children/GalleryScreen/GalleryScreen.stories.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
 	import PureGalleryScreen from "./PureGalleryScreen.svelte";
+	import { RULE, SUBJECTS } from "../../../../constants/rule";
+	import { CANVAS } from "../../../../constants/canvas";
+
+	const galleryItems = [...Array(RULE.GAME_MAX_ROUND).keys()].map((_, index) => {
+		const canvas = document.createElement("canvas");
+		canvas.width = CANVAS.FRAME_SIZE;
+		canvas.height = CANVAS.FRAME_SIZE;
+		if (canvas.getContext) {
+			const context = canvas.getContext("2d");
+			context.beginPath();
+			context.fillStyle = "rgb( 255, 255, 255)";
+			context.fillRect(0, 0, canvas.width, canvas.height);
+		}
+		return {
+			subject: SUBJECTS[index],
+			imageBitmap: canvas,
+			score: 0,
+		};
+	});
 </script>
 
 <Meta title="Screens/Gallery" component={PureGalleryScreen} />
@@ -14,7 +33,7 @@
 <Story
 	name="Gallery"
 	args={{
-		galleryItems: [],
+		galleryItems,
 	}}
 />
 
